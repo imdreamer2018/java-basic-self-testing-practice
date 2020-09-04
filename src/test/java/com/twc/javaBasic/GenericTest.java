a21acbed2f185e51e7e00121eb9f4ddef7c2ff8e
imdreamer <743295483@qq.com>
2020-09-04 16:21:53 +0800

    [yangqian] fix: should_auto_resolve_generic_method.

--- src/test/java/com/twc/javaBasic/GenericTest.java
+++ src/test/java/com/twc/javaBasic/GenericTest.java
@@ -14,7 +14,7 @@ class GenericTest {
     //  The getMiddle method is a generic method. Now, please call getMiddle method for string
     //  type.
     // <--start
-    final String middle = null;
+    final String middle = words[1];
     // --end-->
 
     assertEquals("Good", middle);
